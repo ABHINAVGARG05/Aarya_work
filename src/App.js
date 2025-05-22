cde267c6e476cb55523ceb4ab45aa6c214a826c3
Abhinav <abhichhrp@gmail.com>
2025-05-22 23:06:37 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import Navbar from "./components/Navbar";
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import CompetitionsList from './components/pages/Events/CompetitionsList';
 import CompetitionDetails from './components/pages/Events/CompetitionDetails';
+import './App.css';
 
 function App() {
   return (
-    <div className = "App">
+    <div className="App">
       <Router>
-      <Navbar />
-      <Routes>
-      <Route path = '/' exact element={<Home />} />
-      <Route path='/Events/Competitions' element={<CompetitionsList />} />
-          <Route path='/Events/competitions/:id' element={<CompetitionDetails />} />
-      </Routes>
-        </Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Events/Competitions" element={<CompetitionsList />} />
+          <Route path="/Events/Competitions/Events/Competitions/:id" element={<CompetitionDetails />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
